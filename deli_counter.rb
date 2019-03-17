2929bd7bd1fe44b5ac46ea87a7c8ae9e7e0e839b
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-17 21:15:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,3 +20,9 @@ def line(array)
   end 
 end 
 
+ if array.empty? == true 
+    puts "There is nobody waiting to be served!"
+  elsif array.empty? == false 
+    puts "Currently serving #{array.shift}."
+  end 
+end 
