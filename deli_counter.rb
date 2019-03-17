eefc0ac583baa3754c3b86b9efd93c68de009244
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-17 21:06:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,5 +4,5 @@ def take_a_number(array, name)
   array.push(name)
   position = array.index(name)
   puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
-  return name, position 
+  name, position 
 end 
