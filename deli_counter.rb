4b57a6c6edc3374657ba06ce5cc64bdf17f65608
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-17 21:03:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 
-def take_a_number(katzDeliLine,name) {
+def take_a_number(katz_deli, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
